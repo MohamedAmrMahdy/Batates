dd91fed8f61c4e57d1635a2a3631cfbc1742fdb0
SirAnxious <SirAnxiousALot@gmail.com>
2024-02-20 23:05:35 +0200

    refactor: remove redundant code

--- Repo/IRepo/IRepository.cs
+++ Repo/IRepo/IRepository.cs
@@ -12,7 +12,6 @@ namespace Batates.Repo.IRepo
     {
         IEnumerable<T> GetAll();
         IEnumerable<T> GetAll(params Expression<Func<T, object>>[] includes);
-        IEnumerable<T> GetAll(Func<T, bool> predict, params Expression<Func<T, object>>[] includes);
         T Get(Expression <Func<T, bool>> predict);
         T Get(Expression<Func<T, bool>> predict, params Expression<Func<T, object>>[] includes);
         int Create(T item);
