4c418cd9465f6ef1dfa54fa9daa288988a56276a
SirAnxious <SirAnxiousALot@gmail.com>
2024-02-20 21:28:20 +0200

    feat: Changes to HomeController
    Add main functionality & Add View Pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Batates.Models;
+using Batates.Repo.IRepo;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,21 +7,36 @@ namespace Batates.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+        private readonly IProductRepository ProductRepo;
+        private readonly ICategoryRepository CategoryRepo;
+        private readonly IRestaurantRepository RestaurantRepo;
+
+        public HomeController(ILogger<HomeController> logger, IProductRepository productRepo, ICategoryRepository categoryRepo, IRestaurantRepository restaurantRepo)
         {
             _logger = logger;
+            ProductRepo = productRepo;
+            CategoryRepo = categoryRepo;
+            RestaurantRepo = restaurantRepo;
         }
 
+        //Home Page - View All Categories + Top Rated Restaurants
         public IActionResult Index()
         {
-            return View();
+            return View(CategoryRepo.GetAll());
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Category(int id)
         {
-            return View();
+            return View(RestaurantRepo.GetAll(r => r.Categories.Any(cat => cat.ID == id), r => r.Categories));
+        }
+
+        public IActionResult Restaurant(int id)
+        {
+
+            return View(ProductRepo.GetAll(p => p.RestaurantID == id));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
