cc6948dec63bd56a81caed987684c18373798bbe
EmadERagheb <emaderagheb@gmail.com>
2024-02-21 20:14:24 +0200

    initial cart

---
+++ Repo/Repo/CartRepository.cs
@@ -0,0 +1,13 @@
+﻿using Batates.Data;
+using Batates.Models;
+using Batates.Repo.IRepo;
+
+namespace Batates.Repo.Repo
+{
+    public class CartRepository : Repository<Cart>, ICartRepository
+    {
+        public CartRepository(ApplicationDbContext dB) : base(dB)
+        {
+        }
+    }
+}
