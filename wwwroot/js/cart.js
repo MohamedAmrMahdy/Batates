5d832ca8732510e41a61aacfb9a1debcfb57b254
SirAnxious <SirAnxiousALot@gmail.com>
2024-02-22 19:57:47 +0200

    refactor: JavaScript in the Restaurant page
    Add another function for clearing Cart (if needed) as well as addition if toastr.js script to page

--- wwwroot/js/cart.js
+++ wwwroot/js/cart.js
@@ -1,21 +1,47 @@
-﻿
+﻿toastr.options = { "positionClass": "toast-bottom-right" };
+function clearCart() {
+
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You have items from another Restaurant\nIf you confirm it will be cleared.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Clear Cart"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax
+                (
+                    {
+                        url: '/Cart/ClearCart',
+                        type: "Delete",
+                        success: function (data) {
+                            toastr.success(data.message);
+                        }
+                    }
+                )
+
+        }
+    });
+}
+
+
     $(document).ready(function () {
         $('.addToCartBtn').click(function () {
             var id = $(this).data('id');
-            var restid = $(this).data('restid');
 
             $.ajax({
                 url: '/Cart/Add',
                 type: 'POST',
                 data: {
                     id: id,
-                    restid: restid
                 },
                 success: function (response) {
                     if (response.success) {
-                        alert("Hello" + response.message);
+                        toastr.success(response.message);
                     } else {
-                        alert('Error: ' + response.message);
+                            clearCart();
                     }
                 },
                 error: function () {
